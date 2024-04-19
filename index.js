d235fce0449a1f36971e33130786cde846aad82a
Shihab585203 <freelancersehab1@gmail.com>
2024-04-19 11:47:02 +0600

    updated data

--- index.js
+++ index.js
@@ -30,32 +30,6 @@ async function run() {
     const bookingsCollection = client.db("doctorPortal").collection("bookings");
 
 
-    // Use Aggregate to query multiple collection and then merge data
-    // app.get("/appointmentOptions", async (req, res) => {
-    //   const date = req.query.date;
-    //   const query = {};
-    //   const options = await appointmentOptionCollection.find(query).toArray();
-
-    //   // get the bookings of the provided date
-    //   const bookingQuery = { appointmentDate: date };
-    //   const alreadyBooked = await bookingsCollection
-    //     .find(bookingQuery)
-    //     .toArray();
-
-    //   // code carefully :D
-    //   options.map((option) => {
-    //     const optionBooked = alreadyBooked.filter(
-    //       (book) => book.treatment === option.name
-    //     );
-    //     const bookedSlots = optionBooked.map((book) => book.slot);
-    //     const remainingSlots = option.slots.filter(
-    //       (slot) => !bookedSlots.includes(slot)
-    //     );
-    //     option.slots = remainingSlots;
-    //   });
-    //   res.send(options);
-    // });
-
     app.get("/appointmentOptions", async (req, res) => {
       try {
         const date = req.query.date;
